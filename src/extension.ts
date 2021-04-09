fc152a9a29cde78da222f177100d4177ef7a84b0
Bukii <lukas.weingand@gmail.com>
2021-04-10 00:22:07 +0200

    fundamentals done

--- src/extension.ts
+++ src/extension.ts
@@ -14,16 +14,26 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('commit-msg-picker.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		queryNewMessage().then((res:any) => {
-			vscode.window.showInformationMessage(res);
+	let disposable = vscode.commands.registerCommand('commit-msg-picker.nextMessage', () => {
+		queryNewMessage().then((res: any) => {
+			vscode.window.showInformationMessage(`Your commit message: ${res}`);
 		});
 	});
 
+	// create a new status bar item that we can now manage
+	let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	statusBarItem.command = "commit-msg-picker.nextMessage";
+	statusBarItem.text = "$(megaphone) WhatTheCommit";
+	statusBarItem.tooltip = "Create the random message for your next commit!";
+	statusBarItem.show();
+	
+	context.subscriptions.push(statusBarItem);
 	context.subscriptions.push(disposable);
+
+	// register some listener that make sure the status bar 
+	// item always up-to-date
+	context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(() => statusBarItem.show()));
+	context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(() => statusBarItem.show()));
 }
 
 // this method is called when your extension is deactivated
